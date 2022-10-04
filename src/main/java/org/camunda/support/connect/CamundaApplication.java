40c12c2e054617125ce284938c60e67624d39353
Alessandro Cavalli <alessandro.cavalli@camunda.com>
2022-10-04 22:13:52 +0200

    firts commit

---
+++ src/main/java/org/camunda/support/connect/CamundaApplication.java
@@ -0,0 +1,16 @@
+package org.camunda.support.connect;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication("connect")
+public class CamundaApplication {
+
+  public static void main(String... args){
+    SpringApplication.run(CamundaApplication.class, args);
+
+  }
+
+}
